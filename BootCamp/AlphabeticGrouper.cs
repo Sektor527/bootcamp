02200377430e5ad27e005890b40b8fcbf2bffca3
Wim Vanherle <wim.vanherle@telenet.be>
2011-08-02 12:30:56 +0200

    Created alphabetic grouper class

---
+++ BootCamp/AlphabeticGrouper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BootCamp
+{
+	internal class AlphabeticGrouper
+	{
+		public int MinimumItemsPerGroup { get; set; }
+
+		public List<string> Group(List<string> elements)
+		{
+			Dictionary<string, int> groups = new Dictionary<string, int>();
+
+			ExtractLetters(elements, groups);
+
+			return GroupLetters(groups);
+		}
+
+		private void ExtractLetters(List<string> elements, Dictionary<string, int> groups)
+		{
+			foreach (string element in elements)
+			{
+				string letter = element.Substring(0, 1).ToUpper();
+				if (groups.ContainsKey(letter))
+					groups[letter]++;
+				else
+					groups[letter] = 1;
+			}
+		}
+
+		private List<string> GroupLetters(Dictionary<string, int> groups)
+		{
+			List<string> result = new List<string>();
+
+			KeyValuePair<string, int> regroup = new KeyValuePair<string, int>();
+
+			foreach (KeyValuePair<string, int> group in groups)
+			{
+				regroup = MergeGroups(regroup, group);
+
+				if (regroup.Value >= MinimumItemsPerGroup)
+				{
+					result.Add(regroup.Key);
+					regroup = new KeyValuePair<string, int>();
+					continue;
+				}
+			}
+
+			if (regroup.Key != null)
+				result.Add(regroup.Key);
+
+			return result;
+		}
+
+		private KeyValuePair<string, int> MergeGroups(KeyValuePair<string, int> regroup, KeyValuePair<string, int> group)
+		{
+			if (regroup.Key == null)
+				return group;
+
+			if (regroup.Key.Substring(0,1).CompareTo(group.Key) < 0)
+				return new KeyValuePair<string, int>(regroup.Key.Substring(0, 1) + "-" + group.Key, regroup.Value + group.Value);
+			else
+				return new KeyValuePair<string, int>(group.Key + "-" + regroup.Key.Substring(0, 1), regroup.Value + group.Value);
+		}
+	}
+}
---
+++ BootCampTests/AlphabeticGrouperTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BootCamp;
+using NUnit.Framework;
+
+namespace BootCampTests
+{
+	[TestFixture]
+	public class AlphabeticGrouperTests
+	{
+		[Test]
+		public void GroupingWithOne_NoDuplicates()
+		{
+			AlphabeticGrouper grouper = new AlphabeticGrouper {MinimumItemsPerGroup = 1};
+
+			List<string> groups = grouper.Group(new List<string>(){"aaa", "bbb", "ccc", "ddd", "eee"});
+
+			Assert.AreEqual(5, groups.Count);
+			Assert.Contains("A", groups);
+			Assert.Contains("B", groups);
+			Assert.Contains("C", groups);
+			Assert.Contains("D", groups);
+			Assert.Contains("E", groups);
+		}
+
+		[Test]
+		public void GroupingWithOne_Duplicates()
+		{
+			AlphabeticGrouper grouper = new AlphabeticGrouper {MinimumItemsPerGroup = 1};
+
+			List<string> groups = grouper.Group(new List<string>(){"aaa", "aaaaa", "ccc", "b", "aa"});
+
+			Assert.AreEqual(3, groups.Count);
+			Assert.Contains("A", groups);
+			Assert.Contains("B", groups);
+			Assert.Contains("C", groups);
+		}
+
+		[Test]
+		public void GroupingWithTwo_NoDuplicates()
+		{
+			AlphabeticGrouper grouper = new AlphabeticGrouper {MinimumItemsPerGroup = 2};
+
+			List<string> groups = grouper.Group(new List<string>() {"aaa", "bbb", "ccc", "ddd", "eee"});
+
+			Assert.AreEqual(3, groups.Count);
+			Assert.Contains("A-B", groups);
+			Assert.Contains("C-D", groups);
+			Assert.Contains("E", groups);
+		}
+
+		[Test]
+		public void GroupingWithTwo_Duplicates()
+		{
+			AlphabeticGrouper grouper = new AlphabeticGrouper {MinimumItemsPerGroup = 2};
+
+			List<string> groups = grouper.Group(new List<string>() {"aaa", "aaaaa", "ccc", "b", "aa"});
+
+			Assert.AreEqual(2, groups.Count);
+			Assert.Contains("A", groups);
+			Assert.Contains("B-C", groups);
+		}
+	}
+}
