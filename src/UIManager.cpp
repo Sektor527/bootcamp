f0f5b365f7b758b1b3c9683e0e52b8f1043ac584
Wim Vanherle <wim.vanherle@telenet.be>
2013-02-14 16:41:02 +0100

    [boot-31] Showing rudimentary list of games

--- src/UIManager.cpp
+++ src/UIManager.cpp
@@ -1,11 +1,15 @@
 #include "UIManager.h"
 #include "Controller.h"
 #include <curses.h>
+#include <sstream>
 
 UIManager::UIManager()
 : _controller(NULL)
 {
 	initscr();
+	raw();
+	noecho();
+	keypad(stdscr, true);
 }
 
 UIManager::~UIManager()
@@ -20,5 +24,22 @@ void UIManager::setController(Controller* controller)
 
 void UIManager::start()
 {
+	refresh();
+
+	std::stringstream s;
+	s << _controller->getRowCount() << " games:";
+
+	mvprintw(1, 5, s.str().c_str());
+	for (int i = 1; i <= _controller->getRowCount(); ++i)
+	{
+		mvprintw(i+2, 1, _controller->getID(i).c_str());
+		mvprintw(i+2, 7, _controller->getName(i).c_str());
+		mvprintw(i+2, 60, _controller->getCategory(i).c_str());
+	}
+
+	refresh();
 	getch();
+
+	clear();
+	refresh();
 }
